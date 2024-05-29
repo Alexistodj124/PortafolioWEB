9d40be6bb6dc8ff01b4b5dc111dd9aece765d1b8
Alexistodj124 <alexistodj@gmail.com>
2024-05-29 03:56:11 -0600

    final

---
+++ script.js
@@ -0,0 +1,14 @@
+var typed = new Typed('.multiple-text', {
+    strings: ['Frontend Developer', 'Backend Developer', 'Emprendedor', 'Dueño de negocios', 'Wakeboarder'],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+  });
+
+var typed = new Typed('.text', {
+    strings: ['Ingeniero en Ciencias de la Computacion y Tecnologias de la Informacion, egresado de la distinguida Universidad del Valle de Guatemala.'],
+    frontDelay: 10000,
+    typeSpeed: 50,
+    loop: false
+  });
